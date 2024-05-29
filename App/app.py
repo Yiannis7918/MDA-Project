2bc7fd1efc0092e271b23f2f0b77d6c3a0ff3db4
Yiannis7918 <johnnie.000@hotmail.com>
2024-05-29 17:55:48 +0200

    Final App and Other Files Structure

--- App/app.py
+++ App/app.py
@@ -4,7 +4,8 @@ from dash import Dash, html, dcc
 app = Dash(__name__, use_pages=True)
 
 app.layout = html.Div([
-    html.H1('Multi-page app with Dash Pages'),
+    html.H1("Exploration & Analysis of AED Locations in Relation to Emergency Services and Cardiac Arrests in Belgium"
+            ,style={'text-align':'center'}),
     html.Div([
         html.Div(
             dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
